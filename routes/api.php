c6b33654c70ad0eaacbb848cc6af1874ed189715
RealSyferX <superlevel2021@gmail.com>
2024-08-08 11:35:05 +0800

    ye

--- routes/api.php
+++ routes/api.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "api" middleware group. Make something great!
 | Anime Checkers
+| Testing Pull Request from VSCODE
 */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
